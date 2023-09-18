b5260b391393a8c89cc53716c60773635c8dba37
Adarsh Pandey <97337633+codeswithadarsh@users.noreply.github.com>
2023-09-18 11:03:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,18 @@
 
 function palindrome(str){
 
+	 const len = str.length;
+
+    // loop through half of the string
+    for (let i = 0; i < len / 2; i++) {
+
+        // check if first and last string are same
+        if (str[i] !== str[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
 }
 module.exports = palindrome
